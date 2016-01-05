05850c07e003bc391fe6ab721fe0fbcca57ab4bd
Ezekiel Kigbo <theatlasroom@gmail.com>
2016-01-05 15:44:40 +1100

    updated todos

--- routes/todos.js
+++ routes/todos.js
@@ -6,6 +6,9 @@ var fs = require('fs');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   var todos = [
+    {title: 'Create user stories / needs documentation for prototypes'},
+    {title: 'Go through the 18F Methods cards and figure out how to approach next steps for each prototype'},
+    {title: 'Move this all over to asana'},
     {title: 'Hieng to send me: Cherry\'s report, stuart ross - melb uni criminology academic '},
     {title: 'Chase up online adjournments with Gavin'},
     {title: 'Spin off the NJC intranet interface into a repo'},
@@ -15,11 +18,11 @@ router.get('/', function(req, res, next) {
     {title: 'Talk to @rustum about DOE slack'},
     {title: 'Clean up referrals map'},
     {title: 'Make referrals map + FV prettier'},
-    {title: 'Catch up with Jurgen'},
-    {title: 'Catch up with Alexar (CfA slack)'},
+    {title: 'Organise catch up with Alexar (CfA slack)'},
   ];
 
   var done = [
+    {title: 'Organise catch up with Jurgen'},
     {title: 'set up njc slack'},
   ];
 
