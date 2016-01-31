baad89fbc305f0a71792e282433fd8ace01055fd
Ezekiel Kigbo <theatlasroom@gmail.com>
2016-01-31 23:25:11 +1100

    added robots txt

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var robots = require('robots.txt');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -29,9 +28,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Pass in the absolute path to your robots.txt file
-app.use(robots(__dirname + '/robots.txt'));
 
 app.use('/', routes);
 app.use('/users', users);
